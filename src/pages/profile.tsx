c3daba0ecbfc058c709235f34114a195d6b146bb
marcel-rf <marcelr01@gmail.com>
2022-04-26 11:45:56 -0300

    Style home page. Add custom fonts. Add qr icon.

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import numeral from 'numeral';
 
 import BTCIcon from '../assets/images/btcIcon.svg';
+import QRIcon from '../assets/images/qRCodeIcon.svg';
 import DollarIcon from '../assets/images/dollarIcon.svg';
 
 export const getFormat = (prefix: string, suffix: string, digits: number = 0) => {
@@ -16,68 +17,58 @@ export const getFormat = (prefix: string, suffix: string, digits: number = 0) =>
   return formatArray.join('');
 };
 
-export const formatValues = (value1: number, value2: number, prefix: string, suffix: string, comma: number = 0) => {
-  let format = getFormat(prefix, suffix, comma);
-
-  if ((!value1 && !value2) || value1 === value2) {
-    return [numeral(value1).format(format), numeral(value2).format(format)];
-  }
-  let digits = comma;
-
-  while (true) {
-    format = getFormat(prefix, suffix, digits);
-
-    const val1 = numeral(value1).format(format);
-    const val2 = numeral(value2).format(format);
-
-    const valNum1 = Number(val1.replace(/\D/g, ''));
-    const valNum2 = Number(val2.replace(/\D/g, ''));
-
-    if (valNum1 && valNum2 && val1 !== val2) {
-      return [val1, val2];
-    }
-
-    digits += 1;
-
-    if (digits > 10) {
-      format = getFormat(prefix, suffix, comma);
-      return [numeral(value1).format(format), numeral(value2).format(format)];
-    }
-  }
-};
+// export const formatValues = (value1: number, value2: number, prefix: string, suffix: string, comma: number = 0) => {
+//   let format = getFormat(prefix, suffix, comma);
+//
+//   if ((!value1 && !value2) || value1 === value2) {
+//     return [numeral(value1).format(format), numeral(value2).format(format)];
+//   }
+//   let digits = comma;
+//
+//   while (true) {
+//     format = getFormat(prefix, suffix, digits);
+//
+//     const val1 = numeral(value1).format(format);
+//     const val2 = numeral(value2).format(format);
+//
+//     const valNum1 = Number(val1.replace(/\D/g, ''));
+//     const valNum2 = Number(val2.replace(/\D/g, ''));
+//
+//     if (valNum1 && valNum2 && val1 !== val2) {
+//       return [val1, val2];
+//     }
+//
+//     digits += 1;
+//
+//     if (digits > 10) {
+//       format = getFormat(prefix, suffix, comma);
+//       return [numeral(value1).format(format), numeral(value2).format(format)];
+//     }
+//   }
+// };
 
 export const ProfilePage = () => {
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <div className="w-1/2 min-w-max bg-white bg-opacity-25 border-2 border-secondary rounded-16xl shadow-md p-10">
-        <p className="text-white text-center">Member Stats</p>
+    <div className="w-full h-full flex justify-center">
+      <div className="w-1/2 min-w-max shadow-md p-10">
+        <img className="w-full p-10" src={QRIcon} alt="" />
+        <p className="text-white text-center font-bold">Scan QR Code</p>
         <div className="mt-4">
-          <p className="text-center text-white">Value of Assets you have Deposited</p>
-          <div className="flex items-center justify-center mt-2">
-            <div className="flex items-center justify-center bg-white text-white bg-opacity-25 py-1 px-2 rounded">
-              <img className="w-8 h-8 mr-2" src={BTCIcon} alt="" />
-              {numeral(2.3434 || 0).format('0,0.00')}
-            </div>
-            <div className="flex items-center justify-center bg-white text-white bg-opacity-25 py-1 px-2 rounded ml-6">
-              <img className="w-8 h-8 mr-2" src={DollarIcon} alt="" />
-              {numeral(3434.3 || 0).format('0,0')}
-            </div>
-          </div>
+          <p className="text-center text-white text-sm" style={{fontFamily: 'Righteous', fontStyle: 'normal',}}>
+              Scan the qRCode provided by the store to checkout</p>
+
         </div>
-        <div className="mt-10">
-          <div className="flex items-center justify-around px-10 mt-4">
-            <div className="py-2 px-4 rounded-xl bg-white bg-opacity-25">
-              <p className="text-white">Then</p>
-              <p className="text-white">Now</p>
-              <p className="text-white">IRR*</p>
-            </div>
-            <div className="flex py-2 px-4 rounded-xl bg-white bg-opacity-25">
-              <img className="w-8 h-8 mr-4" src={BTCIcon} alt="" />
-              <div>
-              </div>
-            </div>
-            <div className="flex py-2 px-4 rounded-xl bg-white bg-opacity-25">
-              <img className="w-8 h-8 mr-4" src={DollarIcon} alt="" />
+        <div className="flex items-center justify-center mt-10">
+          <div className="w-full flex flex-col items-center justify-center bg-white text-white bg-opacity-10 py-1 px-2 rounded-10xl">
+            <p style={{fontFamily: 'Righteous', fontStyle: 'normal',}}
+               className="text-white text-start text-xs mr-2 mt-2">Current Balance</p>
+            <div className="flex items-center">
+              <p className="text-white text-center font-bold">
+                <p style={{fontSize: "xx-large", fontFamily: 'Montserrat', fontStyle: 'normal',}} >{numeral(2.3434 || 0).format('0,0.00')}</p>
+              </p>
+              <p style={{fontFamily: 'Righteous', fontStyle: 'normal',}} className="text-white text-center font-bold text-sm pt-2 ml-1">
+                USD
+              </p>
             </div>
           </div>
         </div>
