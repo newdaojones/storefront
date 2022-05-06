e36d0b7645198a4cd39c94479336330deb77a604
marcel-rf <marcelr01@gmail.com>
2022-05-06 09:36:20 -0300

    Add currency convertion helper class.

---
+++ src/helpers/currency.ts
@@ -0,0 +1,37 @@
+import {ITicker} from "../models";
+
+function findETHTicker(tickers: ITicker[]) {
+    let ethTicker = null;
+    try {
+        ethTicker = tickers ? tickers.find(value => value.currency === 'ETH') : null;
+    } catch (e) {
+        console.info(`error searching for ticker ETH: ${e}`)
+    }
+    return ethTicker;
+}
+
+export const convertETHtoUSD = (value: number, tickers: ITicker[]): number | null => {
+    let ethTicker = findETHTicker(tickers);
+    let balanceUsd: number | null;
+    if (ethTicker) {
+        balanceUsd = value * ethTicker.price;
+        console.info(`${value} ETH -> ${balanceUsd} USD. price eth: ${ethTicker?.price}`)
+    } else {
+        console.info(`tickers are not available`)
+        balanceUsd = null;
+    }
+    return balanceUsd;
+};
+
+export const convertUSDtoETH = (value: number, tickers: ITicker[]): number | null => {
+    let ethTicker = findETHTicker(tickers);
+    let balanceUsd: number | null;
+    if (ethTicker) {
+        balanceUsd = value / ethTicker.price;
+        console.info(`${value} USD -> ${balanceUsd} ETH. price eth: ${ethTicker?.price}`)
+    } else {
+        console.info(`ticker are not available for ETH`)
+        balanceUsd = null;
+    }
+    return balanceUsd;
+};
