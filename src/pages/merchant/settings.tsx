916909264bb293ba89887cfea9274fd817cf372d
marcel-rf <marcelr01@gmail.com>
2022-09-05 11:59:10 -0300

    Add test/mainnet toggle selctor in merchant settings.

--- src/pages/merchant/settings.tsx
+++ src/pages/merchant/settings.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {storefrontPayButton} from "../../StorefrontPaySdk";
 import {useSelector} from "react-redux";
 import {selectMerchantInfo} from "../../store/selector";
 import {getAddressFromAccount} from "@walletconnect/utils";
 import {useWalletConnectClient} from "../../contexts/walletConnect";
 import {ellipseAddress} from "../../helpers";
+import Toggle from "../../components/ui/toggle";
 
 export const SettingsPage = () => {
   const { account} = useWalletConnectClient();
   let merchantInfo = useSelector(selectMerchantInfo);
+  const [enabled, setEnabled] = useState(false);
+
   console.log(`merchant ${merchantInfo} add ${merchantInfo?.memberAddress} ${merchantInfo?.merchantName}`);
   console.log(`merchant Account ${account} add ${getAddressFromAccount(account!!)||""}`)
 
-  let payButton = storefrontPayButton(getAddressFromAccount(account!!) || "", "12323", 0.15);
+  let payButton = storefrontPayButton(getAddressFromAccount(account!!) || "", "1", 0.15);
 
 
   React.useEffect(() => {
     if (account) {
-      payButton = storefrontPayButton(getAddressFromAccount(account), "12323", 0.15);
+      payButton = storefrontPayButton(getAddressFromAccount(account), "1", 0.15);
     }
   }, [account]);
 
@@ -50,20 +53,26 @@ export const SettingsPage = () => {
 
           <div className="w-full flex items-center justify-between mt-10">
             <p className="text-center text-white  mr-8">Fake Money</p>
-            <div className="flex items-center justify-center bg-white text-white bg-opacity-25 py-1 px-2 rounded">
-              {'true'}
+            <div className="flex items-center justify-center bg-white text-white bg-opacity-25 py-1 px-2 rounded" >
+              <label htmlFor="toggle-example" className="flex items-center cursor-pointer relative mb-4" >
+                <input type="checkbox" id="toggle-example" className="sr-only" checked={enabled}/>
+                  <div className="toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-full" onClick={() => {
+                    setEnabled(!enabled);
+                  }}></div>
+                  <span className="ml-3 text-sm text-white">{enabled ?'ON' :'OFF'}</span>
+              </label>
             </div>
           </div>
 
           <div className="w-full flex justify-between mt-10">
-            <p className="text-center text-white  mr-8">Supported Tokens</p>
+            <p className="text-center text-white  mr-8">Supported Chains</p>
             <div className="flex flex-col justify-center bg-white bg-opacity-25 py-1 px-2 rounded">
               <div className="flex items-center justify-end text-white py-1 px-2 rounded">
-                {'ETH'}
+                {'Ethereum'}
                 <input className="m-2" type="checkbox" readOnly={true} value='true'/>
               </div>
               <div className="flex items-center justify-center text-white py-1 px-2 rounded mt-2">
-                {'MATIC'}
+                {'Polygon'}
                 <input className="m-2" type="checkbox" value='true'/>
               </div>
             </div>
