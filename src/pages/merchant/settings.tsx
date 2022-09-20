293c921967a2796080f89317748d0a168d727b49
marcel-rf <marcelr01@gmail.com>
2022-09-20 18:12:51 -0300

    Add reload svg icon

--- src/pages/merchant/settings.tsx
+++ src/pages/merchant/settings.tsx
@@ -6,10 +6,12 @@ import {useWalletConnectClient} from "../../contexts/walletConnect";
 import {ellipseAddress} from "../../helpers";
 import {userAction} from "../../store/actions";
 import {IMerchant} from "../../models";
+import logoIcon from "../../assets/images/logo.svg";
+import {toast} from "react-toastify";
 
 export const SettingsPage = () => {
   const dispatch = useDispatch();
-  const { account} = useWalletConnectClient();
+  const { account, disconnect} = useWalletConnectClient();
   let merchantInfo = useSelector(selectMerchantInfo);
   const [enabled, setEnabled] = useState(true);
   const [polygonEnabled, setPolygonEnabled] = useState(true);
@@ -17,7 +19,18 @@ export const SettingsPage = () => {
   console.log(`merchant ${merchantInfo} add ${merchantInfo?.memberAddress} ${merchantInfo?.merchantName}`);
   console.log(`merchant Account ${account} add ${getAddressFromAccount(account!!)||""}`)
 
-  //let payButton = storefrontPayButton(getAddressFromAccount(account!!) || "", "1", 0.15);
+  const onDisconnect = () => {
+    console.info(`onDisconnect called`);
+    toast.info("Disconnecting...", {autoClose: 1000})
+    disconnect().then(r => console.info(`disconnected!`));
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
+  };
+
+  const logoutButton =  <button onClick={onDisconnect} className="flex bg-white justify-center items-center rounded-10xl border border-solid border-t-2 border-slate-800 overflow-hidden mt-4">
+    <p className="font-righteous">Logout</p>
+  </button>
 
   React.useEffect(() => {
     if (account) {
@@ -113,11 +126,13 @@ export const SettingsPage = () => {
             </div>
           </div>
 
+
           <button className="flex bg-white justify-center items-center rounded-10xl border border-solid border-t-2 border-slate-800 overflow-hidden mt-4"
                   onClick={onSaveSettings}>
             <p className="font-righteous">Save</p>
           </button>
 
+          {logoutButton}
           {/*Storefront Pay Button*/}
           {/*<div className="w-full flex flex-col items-center justify-around pt-8">*/}
           {/*  <p className="font-montserrat text-white text-center text-sm mt-2">Add a Pay with Storefront button to your site, or use our sdk.</p>*/}
