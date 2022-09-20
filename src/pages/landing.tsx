177c880e71adecf37a52ad817c8ac8f91b472930
marcel-rf <marcelr01@gmail.com>
2022-09-20 18:14:21 -0300

    Use the waiting for signature state to deliver a proper message in the loading sc

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-export const Landing = () => (
-  <div className="flex flex-col items-center direction-column text-center justify-center h-full w-full">
-    <div className="w-20 h-20">
-        <div className="thecube w-20 h-20 m-1">
-            <div className="cube c1"></div>
-            <div className="cube c2"></div>
-            <div className="cube c4"></div>
-            <div className="cube c3"></div>
+export const Landing = ({text = 'Loading....',}: {
+    text?: string;
+}) => (
+    <div className="flex flex-col items-center direction-column text-center justify-center h-full w-full">
+        <div className="w-20 h-20">
+            <div className="thecube w-20 h-20 m-1">
+                <div className="cube c1"></div>
+                <div className="cube c2"></div>
+                <div className="cube c4"></div>
+                <div className="cube c3"></div>
+            </div>
         </div>
+        <p className="mt-10 text-white text-sm">{text}</p>
     </div>
-    <p className="mt-10 text-white text-sm">Loading...</p>
-  </div>
 );
