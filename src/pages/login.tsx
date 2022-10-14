3988dc4988fb2ce7c4f55c4887d7152f68a23401
marcel-rf <marcelr01@gmail.com>
2022-10-14 16:58:05 -0300

    remove unused button from login.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,7 +4,7 @@ import logoIcon from '../assets/images/logo.svg';
 import backpackLogo from '../assets/images/backpacklogo.png';
 import {useWalletConnectClient} from '../contexts/walletConnect';
 import {toast} from "react-toastify";
-import {storefrontPayButton} from "../StorefrontPaySdk";
+
 
 export const Login = () => {
     const {qrCodeUri} = useWalletConnectClient();
@@ -57,9 +57,11 @@ export const Login = () => {
     const wcV2Deeplink = qrCodeUri ? qrCodeUri : '';
     const backpackDeeplink = qrCodeUri ? `ndj-backpack://wc?uri=${qrCodeUri}` : '';
     const backpackUniversalLink = qrCodeUri ? `https://jxndao.com/wc?uri=${qrCodeUri}` : '';
+    //const androidIntentLink = "intent://wc/#Intent;scheme=wc;package=com.ndj.wallet;end";
+
     return (
         <div className="w-full h-screen flex items-center justify-center flex-col">
-            <a href={wcV2Deeplink} target={"_blank"} className="">
+            <a href={wcV2Deeplink} target={"_blank"} className="" rel={"noreferrer"}>
                 <div className="flex items-center justify-center pt-2">
                     <div id="qrcode" className="flex items-center justify-center rounded-10xl overflow-hidden qrcode">
                     </div>
@@ -70,18 +72,19 @@ export const Login = () => {
                className="text-white text-xs mt-1 mb-8 cursor-pointer">Copy link</p>
             <p className="font-montserrat text-center text-white mt-2 mb-10">Scan or Tap the QrCode <br/>to connect with WalletConnect v2</p>
 
-            {/*Backpack Button*/}
-            <a href={backpackDeeplink} target={"_blank"}>
+            {/*Backpack local Deeplink Button*/}
+            {/*<a href={backpackDeeplink} target={"_blank"} rel={"noreferrer"}>*/}
+            {/*    <button className="flex bg-white justify-center items-center rounded-10xl border border-solid border-t-2 border-slate-800 overflow-hidden mt-4">*/}
+            {/*        <img className="w-8 h-8 mr-4" src={backpackLogo} alt="" />*/}
+            {/*        <p className="font-righteous">Connect with Backpack</p>*/}
+            {/*    </button>*/}
+            {/*</a>*/}
+
+            {/*Backpack Universal Link Button */}
+            <a href={backpackUniversalLink} target={"_blank"} rel={"noreferrer"}>
                 <button className="flex bg-white justify-center items-center rounded-10xl border border-solid border-t-2 border-slate-800 overflow-hidden mt-4">
                     <img className="w-8 h-8 mr-4" src={backpackLogo} alt="" />
                     <p className="font-righteous">Connect with Backpack</p>
-                </button>
-            </a>
-            {/*Backpack Button 2*/}
-            <a href={backpackUniversalLink} target={"_blank"}>
-                <button className="flex bg-white justify-center items-center rounded-10xl border border-solid border-t-2 border-slate-800 overflow-hidden mt-4">
-                    <img className="w-8 h-8 mr-4" src={backpackLogo} alt="" />
-                    <p className="font-righteous">Connect with Backpack 2</p>
                 </button>
             </a>
             <p className="font-montserrat text-center text-white mt-2 mb-10">Or connect directly with your Backpack</p>
