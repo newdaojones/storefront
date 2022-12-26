ebae3f9ed54f969740a459944d8f84927968b56d
marcel-rf <marcelr01@gmail.com>
2022-12-26 16:09:38 -0300

    move defaults to prod/test

--- src/config/appconfig.tsx
+++ src/config/appconfig.tsx
@@ -2,21 +2,20 @@
 const DEVELOPMENT = 0;
 const TEST = 1;
 
-const MAINNET_MODE = false;
+const MAINNET_MODE = true;
 
-// export const BUILD_CONFIG = DEVELOPMENT;
-const BUILD_CONFIG = TEST;
+// export const BACKEND_API_CONFIG = DEVELOPMENT;
+const BACKEND_API_CONFIG = TEST;
 
 // @ts-ignore
 // read only
-export const isDevMode = () => BUILD_CONFIG === DEVELOPMENT;
-
+export const isDevMode = () => BACKEND_API_CONFIG === DEVELOPMENT;
 export const isTestnetMode = () => !MAINNET_MODE;
 
 const ethereumGorliTestnetRpcUrl = "https://goerli.infura.io/v3/f785cca3f0854d5a9b04078a6e380b09";
 const ethereumMainnetRpcUrl = 'https://mainnet.infura.io/v3/cd5b0778994b4e34b166f2569a1166c0';
 
-//TODO Move to move to another INFURA account, since polygon mumbai api requires credit card
+//TODO Move to another INFURA account, since polygon mumbai api requires credit card
 const polygonTestnetRpcUrl = "https://matic-testnet-archive-rpc.bwarelabs.com/";
 const polygonMainnetRpcUrl = "https://rpc-mainnet.matic.network";
 
